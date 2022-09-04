55413bfd903a206961b51873eaee06800d924fb1
Enrique Aspera <enrique.alejandro.aspera@gmail.com>
2022-09-04 23:05:08 +0100

    Added proposed solution to the js file

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -12,3 +12,17 @@ const repeatString = function (string, num) {
 
 // Do not edit below this line
 module.exports = repeatString;
+
+// Proposed solution
+/*
+const repeatString = function(word, times) {
+  if (times < 0) return 'ERROR';
+  let string = '';
+  for (let i = 0; i < times; i++) {
+    string += word;
+  }
+  return string;
+};
+
+module.exports = repeatString;
+*/
